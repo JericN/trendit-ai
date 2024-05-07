1750146197da59c95d14b7372fb7473e2ffbeae6
Jeric <jmnarte@up.edu.ph>
2024-05-08 00:26:38 +0800

    feat: add argument routing

---
+++ main.py
@@ -0,0 +1,23 @@
+""" This is the main script that routes the arguments to the correct function """
+
+import argparse
+from scraper.main import run_reddit_scraper  # Import the function from script/main.py
+
+
+def main():
+    """Main function that routes the arguments to the correct function"""
+    parser = argparse.ArgumentParser(
+        description="This is a python scraper and machine learning model."
+    )
+    parser.add_argument("-s", "--scraper", action="store_true", help="Run the scraper")
+    parser.add_argument("-m", "--model", action="store_true", help="Run the model")
+    args = parser.parse_args()
+
+    if args.scraper:
+        run_reddit_scraper()
+    if args.model:
+        print("Goodbye World!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ scraper/main.py
@@ -0,0 +1,5 @@
+""" This is the main module for the scraper """
+
+def run_reddit_scraper():
+    """Run the Reddit scraper"""
+    print("Running Reddit scraper")
