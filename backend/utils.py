3cd996603bd186a661a1b43644f5dd58147079bd
Jeric <jmnarte@up.edu.ph>
2024-06-03 05:16:49 +0800

    feat: implement weekly scraping service

---
+++ backend/utils.py
@@ -0,0 +1,18 @@
+"""Utility functions for the server"""
+
+from os import path
+import pandas as pd
+
+
+def write_to_csv(data: pd.DataFrame, file_path: str) -> None:
+    """Writes data to a CSV file, appending if the file already exists"""
+    if path.isfile(file_path) and path.getsize(file_path) > 0:
+        data.to_csv(file_path, mode="a", index=False, header=False)
+    else:
+        data.to_csv(file_path, mode="a", index=False, header=True)
+
+
+def read_subreddit_list() -> list[str]:
+    """Reads a list of subreddits from a file"""
+    with open("subreddits.txt", "r", encoding="utf-8") as file:
+        return [sub.strip() for sub in file.readlines()]
